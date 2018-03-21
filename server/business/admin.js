173f310bfbea004c651ebbaadb8a2dc8e61b8f92
Gina <gina@ginazampino.com>
2018-03-21 19:15:30 -0400

    Populated dropdowns.

--- server/business/admin.js
+++ server/business/admin.js
@@ -1,13 +1,83 @@
 const mysql = require('../mysql');
 
-module.exports.getBreeds = async function () {
+module.exports.getBreeds = async function() {
     const sql = `
-        SELECT * FROM pet_breeds ORDER BY breed_name;
+        SELECT * 
+        FROM pet_breeds 
+        ORDER BY breed_name;
     `;
     let conn = await mysql.connect();
     return await conn.query(sql);
 };
 
-module.exports.getGenders = function () {
+module.exports.getGenders = function() {
     return ['Male', 'Female'];
+};
+
+module.exports.getOrigins = function() {
+    return ['Adoption Center', 'Application', 'Breeding', 'Freebie', 'Gift', 'Grab Bag', 'Hexing', 'Sale', 'Trade'];
+};
+
+module.exports.getStatuses = function() {
+    return ['Not Applicable', 'Active', 'Inactive', 'Retired'];
+};
+
+module.exports.getPetPages = async function() {
+    const sql = `
+        SELECT * 
+        FROM pages 
+        WHERE id=1 
+        OR id=2 
+        OR id=8 
+        OR id=9;
+    `;
+    let conn = await mysql.connect();
+    return await conn.query(sql);
+};
+
+module.exports.getLitters = async function() {
+    const sql = `
+        SELECT *
+        FROM litters
+        ORDER BY litter_name;
+    `;
+    let conn = await mysql.connect();
+    return await conn.query(sql);
+};
+
+module.exports.getDownloadPages = async function() {
+    const sql = `
+        SELECT *
+        FROM pages
+        WHERE id=7
+        OR id=11
+        OR id=12
+        OR id=10;
+    `;
+    let conn = await mysql.connect();
+    return await conn.query(sql);
+};
+
+module.exports.getProjectCategories = function() {
+    return ['Hexing Project', 'Breeding Project'];
+};
+
+module.exports.getProjectStatuses = function() {
+    return ['Open', 'Limited', 'Closed'];
+};
+
+module.exports.getShowVenues = function() {
+    return ['Kasual Kennels (KK)', 'RKC Petz Forum (RKC)', 'Whiskerwick (WW)'];
+};
+
+module.exports.getShowCategories = function() {
+    return ['EBW Pose Show (Dali)', 'EBW Pose Show (Dane)', 'Standard Pose Show (Dali)', 'Standard Pose Show (Dane)', 'Contest'];
+};
+
+module.exports.getPlacements = function() {
+    return ['P', 'BIS', '1', '2', '3', 'HM'];
+};
+
+module.exports.getImageCategories = function() {
+    return ['Albums', 'Awards', 'Community Hubs', 'Fan Sites', 'Free-For-All Stamps', 'Limited Edition Stamps', 'Stamp Collection'];
 };
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -7,28 +7,49 @@ module.exports = function (app) {
     app.get("/test", async function (request, response) {
         response.send({
             pet: { /* TODO: Populate me */ },
-            picklists: {
+            dropdowns: {
                 breeds    : await business.admin.getBreeds(),
                 genders   : business.admin.getGenders()
             }
         });
     });
 
+    /*
+
+        Admin Panel
+
+    */
 
     app.get("/admin", function (request, response) {
         response.render("admin/admin.html", {});
     });
+
+    /*
+    
+        Browse Pets
+        
+    */
     
     app.get("/admin/pets", function (request, response) {
         response.render("admin/admin-pets.html", {});
     });
+
+    /*
+    
+        Edit Pets
+        
+    */
     
     app.get("/admin/edit-pet", async function (request, response) {
         let vm = {
             pet: { /* TODO: Populate me */ },
-            picklists: {
+            dropdowns: {
                 breeds    : await business.admin.getBreeds(),
-                genders   : business.admin.getGenders()
+                genders   : business.admin.getGenders(),
+                origins   : business.admin.getOrigins(),
+                statuses  : business.admin.getStatuses(),
+                petpages  : await business.admin.getPetPages(),
+                litters   : await business.admin.getLitters()
             }
         };
 
@@ -36,7 +57,7 @@ module.exports = function (app) {
             vm.pet = {}; // TODO
         }
         else {
-            vm.pet = {};
+            vm.pet = {}; // TODO
         }
 
         response.render("admin/admin-edit-pet.html", vm);
@@ -54,52 +75,199 @@ module.exports = function (app) {
         }
         response.send({ id });
     });
+
+    /*
     
+        Browse Litters
+        
+    */
+
     app.get("/admin/litters", function (request, response) {
         response.render("admin/admin-litters.html", {});
     });
+
+    /*
+    
+        Edit Litters
+        
+    */
     
     app.get("/admin/edit-litter", function (request, response) {
         response.render("admin/admin-edit-litter.html", {});
     });
+
+    /*
+    
+        Browse Downloads
+        
+    */
     
     app.get("/admin/downloads", function (request, response) {
         response.render("admin/admin-downloads.html", {});
     });
     
-    app.get("/admin/edit-download", function (request, response) {
-        response.render("admin/admin-edit-download.html", {});
+    /*
+    
+        Edit Downloads
+        
+    */
+    
+    app.get("/admin/edit-download", async function (request, response) {
+        let vm = {
+            download: { /* TODO */ },
+            picklists: {
+                downloadpages : await business.admin.getDownloadPages()
+            }
+        };
+
+        if (request.query.id) {
+            vm.download = {}; // TODO
+        }
+        else {
+            vm.download = {}; // TODO
+        }
+
+        response.render("admin/admin-edit-download.html", vm);
     });
+
+    /*
+    
+        Browse Projects
+        
+    */
     
     app.get("/admin/projects", function (request, response) {
         response.render("admin/admin-projects.html", {});
     });
+
+    /*
+    
+        Edit Projects
+        
+    */
     
     app.get("/admin/edit-project", function (request, response) {
-        response.render("admin/admin-edit-project.html", {});
+        let vm = {
+            project: { /* TODO */ },
+            dropdowns: {
+                categories  : business.admin.getProjectCategories(),
+                statuses    : business.admin.getProjectStatuses()
+            }
+        };
+
+        if (request.query.id) {
+            vm.pet = {}; // TODO
+        }
+        else {
+            vm.pet = {}; // TODO
+        }
+
+        response.render("admin/admin-edit-project.html", vm);
     });
+
+    /*
+    
+        Browse Shows
+        
+    */
     
     app.get("/admin/shows", function (request, response) {
         response.render("admin/admin-shows.html", {});
     });
     
+    /*
+    
+        Edit Shows
+        
+    */
+
     app.get("/admin/edit-show", function (request, response) {
-        response.render("admin/admin-edit-show.html", {});
+        let vm = {
+            show: { /* TODO */},
+            dropdowns: {
+                venues      : business.admin.getShowVenues(),
+                categories  : business.admin.getShowCategories()
+            }
+        };
+
+        if (request.query.id) {
+            vm.pet = {}; // TODO
+        }
+        else {
+            vm.pet = {}; // TODO
+        }
+
+        response.render("admin/admin-edit-show.html", vm);
     });
+
+    /*
+    
+        Browse Entries
+        
+    */
     
     app.get("/admin/entries", function (request, response) {
         response.render("admin/admin-entries.html", {});
     });
+
+    /*
+    
+        Edit Entries
+        
+    */
     
     app.get("/admin/edit-entry", function (request, response) {
-        response.render("admin/admin-edit-entry.html", {});
+        let vm = {
+            show: { /* TODO */ },
+            dropdowns: {
+                placements  : business.admin.getPlacements(),
+                venues      : business.admin.getShowVenues(),
+                categories  : business.admin.getShowCategories()
+            }
+        };
+
+        if (request.query.id) {
+            vm.pet = {}; // TODO
+        }
+        else {
+            vm.pet = {}; // TODO
+        }
+
+        response.render("admin/admin-edit-entry.html", vm);
     });
     
+
+    /*
+    
+        Browse Images
+        
+    */
+
     app.get("/admin/images", function (request, response) {
         response.render("admin/admin-images.html", {});
     });
+
+    /*
+    
+        Edit Images
+        
+    */
     
     app.get("/admin/edit-image", function (request, response) {
-        response.render("admin/admin-edit-image.html", {});
+        let vm = {
+            image: { /* TODO */},
+            dropdowns: {
+                categories  : business.admin.getImageCategories()
+            }
+        };
+
+        if (request.query.id) {
+            vm.pet = {}; // TODO
+        }
+        else {
+            vm.pet = {}; // TODO
+        }
+
+        response.render("admin/admin-edit-image.html", vm);
     });
 };
