da2d10cf204f7a9c20fbb42b62531d315a2fa307
Gina <gina@ginazampino.com>
2018-03-24 13:38:39 -0400

    Fixed paths.

--- server/middlewares/index.js
+++ server/middlewares/index.js
@@ -8,12 +8,12 @@ module.exports.install = function (app) {
 };
 
 function addStatic(app) {
-    const pathToStatic = path.resolve(__dirname, "../wwwroot");
+    const pathToStatic = path.resolve(__dirname, "../../wwwroot");
     app.use(express.static(pathToStatic));
 }
 
 function addNunjucks(app) {
-    const pathToViews = path.resolve(__dirname, "../views");
+    const pathToViews = path.resolve(__dirname, "../../views");
     nunjucks.configure(pathToViews, {
         autoescape: true,
         express: app,
